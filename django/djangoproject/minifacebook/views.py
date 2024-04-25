eb9fd19cd33b6db2e26b9a5b4a7ebeb51a075df4
Chenyue-Guo <lovesail0712@gmail.com>
2024-04-24 20:01:13 -0400

    Update views.py

--- django/djangoproject/minifacebook/views.py
+++ django/djangoproject/minifacebook/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 from .models import Status
 
-@cache_page(60 * 15, cache="special_cache")
 def index(request):
     context = {"statuses": Status.objects.order_by('-date_time')[:10]}
     return render(request, "index.html", context)
